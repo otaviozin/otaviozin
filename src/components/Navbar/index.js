adee956f959c2fea92717841a96ca2de2e8ce75f
Etec <etec@etec.com>
2024-03-27 14:32:01 -0300

    Redesign commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,11 @@
 export default function Navbar() {
     return (
-        <header className='navbar'>
-            <nav className='navLogo'>
-                <a href='/'>Dev Otávio</a>
-            </nav>
-            <nav className='navLinks'>
-                <a href='#about'>Sobre</a>
-                <a href='#projects'>Projetos</a>
-                <a href='#contact'>Contato</a>
-            </nav>
-        </header>
+        <nav className='navbar'>
+            <a href='#'>Icon</a>
+            <div className='navLinks'>
+                <a href='#'>Home</a>
+                <a href='#'>Repos</a>
+            </div>
+        </nav>
     );
 }
