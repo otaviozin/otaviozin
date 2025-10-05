8b95af8a7e0bf74fa35bd2ea88d1e4b53ff999b3
Otávio <otaviopereira2004@gmail.com>
2025-10-05 18:16:24 -0300

    feat: added scroll in navigation links. Added english translation

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import '../globals.css';
 import React from 'react';
-import { hasLocale } from 'next-intl';
+import { hasLocale, NextIntlClientProvider } from 'next-intl';
 import { routing } from '@/i18n/routing';
 import { notFound } from 'next/navigation';
 import { Navigation } from '@/components/layout/navigation';
@@ -36,9 +36,10 @@ export default async function RootLayout({ children, params }: Props) {
     <html lang='pt-br' className='dark' suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Particles className='fixed inset-0 -z-10 h-screen w-screen dark:invert' color='#000000' />
-
-        <Navigation />
-        <main className='py-18'>{children}</main>
+        <NextIntlClientProvider>
+          <Navigation />
+          <main className='py-18'>{children}</main>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
