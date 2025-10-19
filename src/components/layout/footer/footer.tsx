360ebbbffb1f470c29d9d02d65163e764c09b67a
Otávio <otaviopereira2004@gmail.com>
2025-10-19 18:25:07 -0300

    feat: added linkedin and twitter in contact

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,6 +1,8 @@
 import { useTranslations } from 'next-intl';
 import { AuroraText } from '../../ui/aurora-text';
 import { ContactForm } from './footer-form';
+import { Linkedin, Twitter } from 'lucide-react';
+import Link from 'next/link';
 
 export const Footer = () => {
   const t = useTranslations('Footer');
@@ -10,12 +12,32 @@ export const Footer = () => {
       id='contact'
       className='grid lg:grid-cols-2 px-6 py-4 lg:px-36 bg-white/80 dark:bg-black/80 border-t border-border backdrop-blur-2xl backdrop-saturate-150 shadow-lg'
     >
-      <aside>
-        <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight text-balance'>
-          <AuroraText colors={['#059669', '#14532d', '#047857', '#065f46']}>{t('title')}</AuroraText>
-        </h1>
-        <p className='text-muted-foreground text-xl'>{t('desc')}</p>
+      <aside className='flex flex-col justify-between h-full'>
+        <div>
+          <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight text-balance'>
+            <AuroraText colors={['#059669', '#14532d', '#047857', '#065f46']}>{t('title')}</AuroraText>
+          </h1>
+          <p className='text-muted-foreground text-xl'>{t('desc')}</p>
+        </div>
+
+        <div className='mt-8 space-y-2'>
+          <Link
+            className='flex gap-2 text-muted-foreground text-xl hover:text-white'
+            href='https://www.linkedin.com/in/otavio-ppereira/'
+          >
+            <Linkedin />
+            Otávio Pereira
+          </Link>
+          <Link
+            className='flex gap-2 text-muted-foreground text-xl hover:text-white'
+            href='https://twitter.com/otaviozinn'
+          >
+            <Twitter />
+            otaviozinn
+          </Link>
+        </div>
       </aside>
+
       <aside className='w-full place-items-center'>
         <ContactForm />
       </aside>
